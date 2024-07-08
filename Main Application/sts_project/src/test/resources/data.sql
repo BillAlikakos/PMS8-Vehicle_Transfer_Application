a05e5676620fd151f2da087f6b5b8ecbbd17b371
BillAlikakos <40693118+BillAlikakos@users.noreply.github.com>
2024-07-08 22:38:22 +0300

    Minor update

--- Main Application/sts_project/src/test/resources/data.sql
+++
@@ -1,5 +0,0 @@
-/*INSERT INTO users_table (username, firstName, lastName, userRole, createdAt, updatedAt)  VALUES('paul_pop', 'paulous', 'cool', 'admin', '2023-06-25','2023-06-25');
-INSERT INTO users_table (username, firstName, lastName, userRole, createdAt, updatedAt)  VALUES('kaySlow', 'Kay', 'Slow', 'admin', '2023-06-25','2023-06-25');
-INSERT INTO users_table (username, firstName, lastName, userRole, createdAt, updatedAt)  VALUES('ghostNinja', 'Ghost', 'Ninja', 'user', '2023-06-25','2023-06-25');
-INSERT INTO users_table (username, firstName, lastName, userRole, createdAt, updatedAt)  VALUES('fastBean', 'Fast', 'Bean', 'user', '2023-06-25','2023-06-25');*/
-INSERT INTO "user_table" ("first_name", "last_name", "user_name", "user_id", "tax_id", "address", "dtype") VALUES ('Yolo', 'Swaggins', 'YoloSwaggins', '1312', '555', 'Nowhere','1');
